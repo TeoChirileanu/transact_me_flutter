b0d06f811fecd8f0a71d35aaf9432ef34b96ba40
Teodor <teodorchirileanu@gmail.com>
2021-02-16 10:00:05 +0200

    add transfer form

---
+++ lib/transfer_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TransferForm extends StatefulWidget {
+  @override
+  _TransferFormState createState() => _TransferFormState();
+}
+
+class _TransferFormState extends State<TransferForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("TODO"),
+    );
+  }
+}
