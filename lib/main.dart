49a439e05e138a1612f70de95d29b2d7231b897e
Teodor <teodorchirileanu@gmail.com>
2021-02-16 09:42:00 +0200

    extract operation form into separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:transact_me/transaction_form.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,58 +13,8 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Transact Me"),
         ),
-        body: OperationForm(),
+        body: TransactionForm(),
       ),
     );
-  }
-}
-
-class OperationForm extends StatefulWidget {
-  final _optiuniDeSelectare = [
-    new DropdownMenuItem(
-      child: Center(child: Text(Operatiuni.Tranzactie)),
-      value: Operatiuni.Tranzactie,
-    ),
-    new DropdownMenuItem(
-      child: Center(child: Text(Operatiuni.Transfer)),
-      value: Operatiuni.Transfer,
-    ),
-  ];
-
-  @override
-  OperationFormState createState() => OperationFormState();
-}
-
-class Operatiuni {
-  static const String Tranzactie = "Tranzacție";
-  static const String Transfer = "Transfer";
-}
-
-class OperationFormState extends State<OperationForm> {
-  var _operatiuneaCurenta = Operatiuni.Tranzactie;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Cel fel de operațiune ați dori să efectuați?"),
-          DropdownButton(
-            value: _operatiuneaCurenta,
-            items: widget._optiuniDeSelectare,
-            onChanged: laSelectareaOperatiunii,
-          ),
-        ],
-      ),
-    );
-  }
-
-  laSelectareaOperatiunii(operatiuneSelectata) {
-    setState(() => _operatiuneaCurenta = operatiuneSelectata);
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text("Ați selectat operațiunea $operatiuneSelectata"),
-      duration: Duration(seconds: 1),
-    ));
   }
 }
